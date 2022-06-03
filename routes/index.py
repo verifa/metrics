34c3208c977d4855be81cc48240a02c39ae0d23b
Thierry Lacour <tlacour@verifa.io>
2022-06-03 11:51:28 +0200

    Distinguish between table and figures in variable names

--- routes/index.py
+++ routes/index.py
@@ -66,14 +66,14 @@ table_rates = ff.create_table(data.ratesTable())
 
 
 # =========================================================
-# Table: Rolling time (individual)
+# Figure: Rolling time (individual)
 # =========================================================
 
 
 d_user_time_rolling = data.userRolling7(["Billable", "Internal"])
 d_user_time_rolling.loc[d_user_time_rolling["Date"] < FIRST_WEEK, "Billable"] = np.nan
 d_user_time_rolling.loc[d_user_time_rolling["Date"] < FIRST_WEEK, "Internal"] = np.nan
-table_rolling_time_individual = px.scatter(
+figure_rolling_time_individual = px.scatter(
     d_user_time_rolling[d_user_time_rolling["Date"] > ROLLING_DATE],
     x="Date",
     y=["Billable", "Internal"],
@@ -81,11 +81,11 @@ table_rolling_time_individual = px.scatter(
     facet_col_wrap=3,
     height=800,
 )
-table_rolling_time_individual.update_layout(title="Rolling 7 days")
+figure_rolling_time_individual.update_layout(title="Rolling 7 days")
 
 
 # =========================================================
-# Table: Rolling time (team)
+# Figure: Rolling time (team)
 # =========================================================
 
 
@@ -95,14 +95,14 @@ d_team_time_rolling_30.columns = ["Date", "Billable30", "Internal30"]
 d_team_time_rolling_30.loc[d_team_time_rolling_30["Date"] < FIRST_MONTH, "Billable30"] = np.nan
 d_team_time_rolling_30.loc[d_team_time_rolling_30["Date"] < FIRST_MONTH, "Internal30"] = np.nan
 d_team_time_rolling_30 = d_team_time_rolling_30.merge(d_team_time_rolling_7, on=["Date"])
-table_rolling_time_team = px.scatter(
+figure_rolling_time_team = px.scatter(
     d_team_time_rolling_30,
     x="Date",
     y=["Billable", "Billable30", "Internal", "Internal30"],
     color_discrete_sequence=["#8FBC8F", "#006400", "#FF7F50", "#A52A2A"],
     height=600,
 )
-table_rolling_time_team.update_layout(
+figure_rolling_time_team.update_layout(
     title="Team average, rolling 7 days, based on time",
     xaxis_rangeslider_visible=True,
     xaxis_range=[ROLLING_DATE, str(date.today())],
@@ -110,13 +110,13 @@ table_rolling_time_team.update_layout(
 
 
 # =========================================================
-# Table: Rolling income (individual)
+# Figure: Rolling income (individual)
 # =========================================================
 
 
 d_user_time_rolling = data.userRolling7("Income")
 d_user_time_rolling.loc[d_user_time_rolling["Date"] < FIRST_WEEK, "Income"] = np.nan
-table_rolling_income_individual = px.scatter(
+figure_rolling_income_individual = px.scatter(
     d_user_time_rolling[d_user_time_rolling["Date"] > ROLLING_DATE],
     x="Date",
     y="Income",
@@ -124,11 +124,11 @@ table_rolling_income_individual = px.scatter(
     facet_col_wrap=3,
     height=800,
 )
-table_rolling_income_individual.update_layout(title="Rolling 7 days (income)")
+figure_rolling_income_individual.update_layout(title="Rolling 7 days (income)")
 
 
 # =========================================================
-# Table: Rolling income (team)
+# Figure: Rolling income (team)
 # =========================================================
 
 
@@ -137,14 +137,14 @@ d_team_time_rolling_30 = rollingAverage(d_team_time_rolling_7, "Income", 30)
 d_team_time_rolling_30.columns = ["Date", "Income30"]
 d_team_time_rolling_30.loc[d_team_time_rolling_30["Date"] < FIRST_MONTH, "Income30"] = np.nan
 d_team_time_rolling_30 = d_team_time_rolling_30.merge(d_team_time_rolling_7, on=["Date"])
-table_rolling_income_team = px.scatter(
+figure_rolling_income_team = px.scatter(
     d_team_time_rolling_30,
     x="Date",
     y=["Income", "Income30"],
     color_discrete_sequence=["#8FBC8F", "#006400"],
     height=600,
 )
-table_rolling_income_team.update_layout(
+figure_rolling_income_team.update_layout(
     yaxis_title="Income (euro)",
     title="Team average, rolling 7 days, based on income",
     xaxis_rangeslider_visible=True,
@@ -153,7 +153,7 @@ table_rolling_income_team.update_layout(
 
 
 # =========================================================
-# Table: Rolling total
+# Figure: Rolling total
 # =========================================================
 
 
@@ -174,14 +174,14 @@ d_team_rolling_total.loc[d_team_rolling_total["Date"] < FIRST_MONTH, "Income30"]
 d_team_rolling_total.loc[d_team_rolling_total["Date"] < FIRST_QUARTER, "Income90"] = np.nan
 d_team_rolling_total.loc[d_team_rolling_total["Date"] < FIRST_YEAR, "Income365"] = np.nan
 
-table_rolling_total = px.scatter(
+figure_rolling_total = px.scatter(
     d_team_rolling_total,
     x="Date",
     y=["Income", "Income30", "Income90", "Income365"],
     color_discrete_sequence=["#C8E6C9", "#81C784", "#388E3C", "#1B5E20"],
     height=600,
 )
-table_rolling_total.update_layout(
+figure_rolling_total.update_layout(
     yaxis_title="Income (euro)",
     title="Weekly income",
     xaxis_rangeslider_visible=True,
@@ -190,12 +190,12 @@ table_rolling_total.update_layout(
 
 
 # =========================================================
-# Table: Projects (individual)
+# Figure: Projects (individual)
 # =========================================================
 
 
 d_by_group = data.byGroup().sort_values("Group")
-table_projects_individual = px.histogram(
+figure_projects_individual = px.histogram(
     d_by_group[d_by_group["Date"] > ROLLING_DATE],
     x="Date",
     y="Time",
@@ -204,64 +204,64 @@ table_projects_individual = px.histogram(
     facet_col_wrap=3,
     height=800,
 )
-table_projects_individual.update_layout(bargap=0.1, title="What do we work on")
+figure_projects_individual.update_layout(bargap=0.1, title="What do we work on")
 
 
 # =========================================================
-# Table: Projects (team)
+# Figure: Projects (team)
 # =========================================================
 
 
 d_by_group = data.byGroup().sort_values("Group")
-table_projects_team = px.histogram(
+figure_projects_team = px.histogram(
     d_by_group[d_by_group["Date"] > ROLLING_DATE], x="Date", y="Time", color="Group", height=600
 )
-table_projects_team.update_layout(bargap=0.1, title="What do we work on")
+figure_projects_team.update_layout(bargap=0.1, title="What do we work on")
 
 
 # =========================================================
-# Table: Billable time
+# Figure: Billable time
 # =========================================================
 
 
-table_billable = px.histogram(data.data.sort_values("Key"), x="User", y="Billable", color="Key", height=600)
+figure_billable = px.histogram(data.data.sort_values("Key"), x="User", y="Billable", color="Key", height=600)
 
-table_billable.update_xaxes(categoryorder="total ascending")
-
-
-# =========================================================
-# Table: Internal time
-# =========================================================
-
-
-table_internal = px.histogram(data.data.sort_values("Key"), x="User", y="Internal", color="Key", height=600)
-
-table_internal.update_xaxes(categoryorder="total descending")
+figure_billable.update_xaxes(categoryorder="total ascending")
 
 
 # =========================================================
-# Table: Popular projects
+# Figure: Internal time
 # =========================================================
 
 
-table_popular_projects = px.histogram(data.data, x="Group", y="Time", color="User", height=600)
+figure_internal = px.histogram(data.data.sort_values("Key"), x="User", y="Internal", color="Key", height=600)
 
-table_popular_projects.update_xaxes(categoryorder="total ascending")
+figure_internal.update_xaxes(categoryorder="total descending")
 
 
 # =========================================================
-# Table: Egg baskets
+# Figure: Popular projects
+# =========================================================
+
+
+figure_popular_projects = px.histogram(data.data, x="Group", y="Time", color="User", height=600)
+
+figure_popular_projects.update_xaxes(categoryorder="total ascending")
+
+
+# =========================================================
+# Figure: Egg baskets
 # =========================================================
 
 
 days_ago = 90
 if supplementary_data.rates.empty:
     yAxisTitle = "Sum of billable time"
-    table_eggbaskets = px.histogram(data.byTotalGroup(days_ago), x="Group", y="Billable", color="User")
-    table_eggbaskets.update_xaxes(categoryorder="total ascending")
+    figure_eggbaskets = px.histogram(data.byTotalGroup(days_ago), x="Group", y="Billable", color="User")
+    figure_eggbaskets.update_xaxes(categoryorder="total ascending")
 else:
     yAxisTitle = "Sum of Income (Euro)"
-    table_eggbaskets = px.histogram(
+    figure_eggbaskets = px.histogram(
         data.byEggBaskets(),
         x="Group",
         y="Income",
@@ -271,7 +271,7 @@ else:
         category_orders={"TimeBasket": ["60-90 days ago", "30-60 days ago", "0-30 days ago"]},
     )
 
-table_eggbaskets.update_layout(
+figure_eggbaskets.update_layout(
     height=600, yaxis_title=yAxisTitle, bargap=0.1, title="Baskets for our eggs (" + str(days_ago) + " days back)"
 )
 
@@ -309,14 +309,14 @@ pageheader = html.Div(
 figure_tabs = {
     "working_hours": [table_working_hours],
     "rates": [table_rates],
-    "billable": [table_billable],
-    "internal": [table_internal],
-    "popular_projects": [table_popular_projects],
-    "projects": [table_projects_team, table_projects_individual],
-    "eggbaskets": [table_eggbaskets],
-    "rolling_time": [table_rolling_time_team, table_rolling_time_individual],
-    "rolling_income": [table_rolling_income_team, table_rolling_income_individual],
-    "rolling_total": [table_rolling_total],
+    "billable": [figure_billable],
+    "internal": [figure_internal],
+    "popular_projects": [figure_popular_projects],
+    "projects": [figure_projects_team, figure_projects_individual],
+    "eggbaskets": [figure_eggbaskets],
+    "rolling_time": [figure_rolling_time_team, figure_rolling_time_individual],
+    "rolling_income": [figure_rolling_income_team, figure_rolling_income_individual],
+    "rolling_total": [figure_rolling_total],
 }
 
 
