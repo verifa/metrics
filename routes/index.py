ca1e2002a3607d78dcc27c2f3f67c11a36cb4863
Thierry Lacour <tlacour@verifa.io>
2022-06-03 10:31:15 +0200

    Pythonify variable naming

--- routes/index.py
+++ routes/index.py
@@ -10,6 +10,11 @@ from dash import dcc, html
 from routes.date_utils import lookAhead, lookBack
 from routes.tempo import SupplementaryData, TempoData
 
+# =========================================================
+# Constants
+# =========================================================
+
+
 START_DATE = pd.Timestamp("2021-01-01")
 TODAY = pd.Timestamp("today")
 FIRST_WEEK = lookAhead(7, START_DATE)
@@ -18,140 +23,180 @@ FIRST_QUARTER = lookAhead(90, START_DATE)
 FIRST_YEAR = lookAhead(365, START_DATE)
 ROLLING_DATE = lookBack(180)
 
-#
+
 # =========================================================
-#
-
-
-def teamRollingAverage7(r7, tomean):
-    dailyAverage = r7.groupby(["Date"])[tomean].mean()
-    return dailyAverage.reset_index(inplace=False)
-
-
-def rollingAverage(dailyData, tomean, days):
-    monthlyAvg = dailyData.set_index("Date").rolling(str(days) + "d")[tomean].mean()
-    return monthlyAvg.reset_index(inplace=False)
-
-
-#
+# Helpers
 # =========================================================
-#
 
-# Fetch the data from tempo
-work = TempoData()
-work.load(from_date=START_DATE, to_date=TODAY)
 
-# read config files
-sd = SupplementaryData(work.getUsers())
+def teamRollingAverage7(frame, to_mean):
+    return frame.groupby(["Date"])[to_mean].mean().reset_index(inplace=False)
 
-# add rate info to data
-work.uprateWork(sd.rates)
 
-# create aggregated table with all users
-table1 = ff.create_table(work.byUser(sd.working_hours))
+def rollingAverage(frame, to_mean, days):
+    return frame.set_index("Date").rolling(str(days) + "d")[to_mean].mean().reset_index(inplace=False)
 
-# create table with all rates
-table2 = ff.create_table(work.ratesTable())
 
-# Rolling individual (time)
-rolling7 = work.userRolling7(["Billable", "Internal"])
-rolling7.loc[rolling7["Date"] < FIRST_WEEK, "Billable"] = np.nan
-rolling7.loc[rolling7["Date"] < FIRST_WEEK, "Internal"] = np.nan
-rollingAll = px.scatter(
-    rolling7[rolling7["Date"] > ROLLING_DATE],
+# =========================================================
+# Fetch data
+# =========================================================
+
+
+data = TempoData()
+data.load(from_date=START_DATE, to_date=TODAY)
+
+supplementary_data = SupplementaryData(data.getUsers())
+data.uprate(supplementary_data.rates)
+
+
+# =========================================================
+# Table: User working hours
+# =========================================================
+
+
+table_working_hours = ff.create_table(data.byUser(supplementary_data.working_hours))
+
+
+# =========================================================
+# Table: Project rates
+# =========================================================
+
+
+table_rates = ff.create_table(data.ratesTable())
+
+
+# =========================================================
+# Table: Rolling time (individual)
+# =========================================================
+
+
+d_user_time_rolling = data.userRolling7(["Billable", "Internal"])
+d_user_time_rolling.loc[d_user_time_rolling["Date"] < FIRST_WEEK, "Billable"] = np.nan
+d_user_time_rolling.loc[d_user_time_rolling["Date"] < FIRST_WEEK, "Internal"] = np.nan
+table_rolling_time_individual = px.scatter(
+    d_user_time_rolling[d_user_time_rolling["Date"] > ROLLING_DATE],
     x="Date",
     y=["Billable", "Internal"],
     facet_col="User",
     facet_col_wrap=3,
     height=800,
 )
-rollingAll.update_layout(title="Rolling 7 days")
+table_rolling_time_individual.update_layout(title="Rolling 7 days")
 
-# Rolling team (time)
-rollingAverage7 = teamRollingAverage7(rolling7, ["Billable", "Internal"])
-teamRollingAverage30 = rollingAverage(rollingAverage7, ["Billable", "Internal"], 30)
-teamRollingAverage30.columns = ["Date", "Billable30", "Internal30"]
-teamRollingAverage30.loc[teamRollingAverage30["Date"] < FIRST_MONTH, "Billable30"] = np.nan
-teamRollingAverage30.loc[teamRollingAverage30["Date"] < FIRST_MONTH, "Internal30"] = np.nan
-teamRollingAverage30 = teamRollingAverage30.merge(rollingAverage7, on=["Date"])
-rollingTeamAverage = px.scatter(
-    teamRollingAverage30,
+
+# =========================================================
+# Table: Rolling time (team)
+# =========================================================
+
+
+d_team_time_rolling_7 = teamRollingAverage7(d_user_time_rolling, ["Billable", "Internal"])
+d_team_time_rolling_30 = rollingAverage(d_team_time_rolling_7, ["Billable", "Internal"], 30)
+d_team_time_rolling_30.columns = ["Date", "Billable30", "Internal30"]
+d_team_time_rolling_30.loc[d_team_time_rolling_30["Date"] < FIRST_MONTH, "Billable30"] = np.nan
+d_team_time_rolling_30.loc[d_team_time_rolling_30["Date"] < FIRST_MONTH, "Internal30"] = np.nan
+d_team_time_rolling_30 = d_team_time_rolling_30.merge(d_team_time_rolling_7, on=["Date"])
+table_rolling_time_team = px.scatter(
+    d_team_time_rolling_30,
     x="Date",
     y=["Billable", "Billable30", "Internal", "Internal30"],
     color_discrete_sequence=["#8FBC8F", "#006400", "#FF7F50", "#A52A2A"],
     height=600,
 )
-rollingTeamAverage.update_layout(
+table_rolling_time_team.update_layout(
     title="Team average, rolling 7 days, based on time",
     xaxis_rangeslider_visible=True,
     xaxis_range=[ROLLING_DATE, str(date.today())],
 )
 
-# Rolling individual (income)
-rollingIncome7 = work.userRolling7("Income")
-rolling7.loc[rolling7["Date"] < FIRST_WEEK, "Income"] = np.nan
-rollingAllIncome = px.scatter(
-    rollingIncome7[rollingIncome7["Date"] > ROLLING_DATE],
+
+# =========================================================
+# Table: Rolling income (individual)
+# =========================================================
+
+
+d_user_time_rolling = data.userRolling7("Income")
+d_user_time_rolling.loc[d_user_time_rolling["Date"] < FIRST_WEEK, "Income"] = np.nan
+table_rolling_income_individual = px.scatter(
+    d_user_time_rolling[d_user_time_rolling["Date"] > ROLLING_DATE],
     x="Date",
     y="Income",
     facet_col="User",
     facet_col_wrap=3,
     height=800,
 )
-rollingAllIncome.update_layout(title="Rolling 7 days (income)")
+table_rolling_income_individual.update_layout(title="Rolling 7 days (income)")
 
-# Rolling team (income)
-rollingAverage7 = teamRollingAverage7(rollingIncome7, "Income")
-teamRollingAverage30 = rollingAverage(rollingAverage7, "Income", 30)
-teamRollingAverage30.columns = ["Date", "Income30"]
-teamRollingAverage30.loc[teamRollingAverage30["Date"] < FIRST_MONTH, "Income30"] = np.nan
-teamRollingAverage30 = teamRollingAverage30.merge(rollingAverage7, on=["Date"])
-rollingTeamAverageIncome = px.scatter(
-    teamRollingAverage30, x="Date", y=["Income", "Income30"], color_discrete_sequence=["#8FBC8F", "#006400"], height=600
+
+# =========================================================
+# Table: Rolling income (team)
+# =========================================================
+
+
+d_team_time_rolling_7 = teamRollingAverage7(d_user_time_rolling, "Income")
+d_team_time_rolling_30 = rollingAverage(d_team_time_rolling_7, "Income", 30)
+d_team_time_rolling_30.columns = ["Date", "Income30"]
+d_team_time_rolling_30.loc[d_team_time_rolling_30["Date"] < FIRST_MONTH, "Income30"] = np.nan
+d_team_time_rolling_30 = d_team_time_rolling_30.merge(d_team_time_rolling_7, on=["Date"])
+table_rolling_income_team = px.scatter(
+    d_team_time_rolling_30,
+    x="Date",
+    y=["Income", "Income30"],
+    color_discrete_sequence=["#8FBC8F", "#006400"],
+    height=600,
 )
-rollingTeamAverageIncome.update_layout(
+table_rolling_income_team.update_layout(
     yaxis_title="Income (euro)",
     title="Team average, rolling 7 days, based on income",
     xaxis_rangeslider_visible=True,
     xaxis_range=[ROLLING_DATE, str(date.today())],
 )
 
-# Weekly income
-rollingIncome7 = work.teamRolling7("Income")
-teamRollingAverage30 = rollingAverage(rollingIncome7, "Income", 30)
-teamRollingAverage30.columns = ["Date", "Income30"]
-teamRollingAverage30 = teamRollingAverage30.merge(rollingIncome7, on=["Date"])
-teamRollingAverage90 = rollingAverage(rollingIncome7, "Income", 90)
-teamRollingAverage90.columns = ["Date", "Income90"]
-teamRollingAverage90 = teamRollingAverage90.merge(teamRollingAverage30, on=["Date"])
-teamRollingAverage365 = rollingAverage(rollingIncome7, "Income", 365)
-teamRollingAverage365.columns = ["Date", "Income365"]
-teamRollingAverage365 = teamRollingAverage365.merge(teamRollingAverage90, on=["Date"])
 
-teamRollingAverage = teamRollingAverage365
-teamRollingAverage.loc[teamRollingAverage["Date"] < FIRST_WEEK, "Income"] = np.nan
-teamRollingAverage.loc[teamRollingAverage["Date"] < FIRST_MONTH, "Income30"] = np.nan
-teamRollingAverage.loc[teamRollingAverage["Date"] < FIRST_QUARTER, "Income90"] = np.nan
-teamRollingAverage.loc[teamRollingAverage["Date"] < FIRST_YEAR, "Income365"] = np.nan
+# =========================================================
+# Table: Rolling total
+# =========================================================
 
-rollingAllIncomeTotal = px.scatter(
-    teamRollingAverage,
+
+d_user_time_rolling = data.teamRolling7("Income")
+d_team_time_rolling_30 = rollingAverage(d_user_time_rolling, "Income", 30)
+d_team_time_rolling_30.columns = ["Date", "Income30"]
+d_team_time_rolling_30 = d_team_time_rolling_30.merge(d_user_time_rolling, on=["Date"])
+d_team_rolling_total_90 = rollingAverage(d_user_time_rolling, "Income", 90)
+d_team_rolling_total_90.columns = ["Date", "Income90"]
+d_team_rolling_total_90 = d_team_rolling_total_90.merge(d_team_time_rolling_30, on=["Date"])
+d_team_rolling_total_365 = rollingAverage(d_user_time_rolling, "Income", 365)
+d_team_rolling_total_365.columns = ["Date", "Income365"]
+d_team_rolling_total_365 = d_team_rolling_total_365.merge(d_team_rolling_total_90, on=["Date"])
+
+d_team_rolling_total = d_team_rolling_total_365
+d_team_rolling_total.loc[d_team_rolling_total["Date"] < FIRST_WEEK, "Income"] = np.nan
+d_team_rolling_total.loc[d_team_rolling_total["Date"] < FIRST_MONTH, "Income30"] = np.nan
+d_team_rolling_total.loc[d_team_rolling_total["Date"] < FIRST_QUARTER, "Income90"] = np.nan
+d_team_rolling_total.loc[d_team_rolling_total["Date"] < FIRST_YEAR, "Income365"] = np.nan
+
+table_rolling_total = px.scatter(
+    d_team_rolling_total,
     x="Date",
     y=["Income", "Income30", "Income90", "Income365"],
     color_discrete_sequence=["#C8E6C9", "#81C784", "#388E3C", "#1B5E20"],
     height=600,
 )
-rollingAllIncomeTotal.update_layout(
+table_rolling_total.update_layout(
     yaxis_title="Income (euro)",
     title="Weekly income",
     xaxis_rangeslider_visible=True,
     xaxis_range=[ROLLING_DATE, str(date.today())],
 )
 
-# Projects personal
-time3data = work.byGroup().sort_values("Group")
-time3 = px.histogram(
-    time3data[time3data["Date"] > ROLLING_DATE],
+
+# =========================================================
+# Table: Projects (individual)
+# =========================================================
+
+
+d_by_group = data.byGroup().sort_values("Group")
+table_projects_individual = px.histogram(
+    d_by_group[d_by_group["Date"] > ROLLING_DATE],
     x="Date",
     y="Time",
     color="Group",
@@ -159,38 +204,65 @@ time3 = px.histogram(
     facet_col_wrap=3,
     height=800,
 )
-time3.update_layout(bargap=0.1, title="What do we work on")
+table_projects_individual.update_layout(bargap=0.1, title="What do we work on")
 
-# Projects team
-time4data = work.byGroup().sort_values("Group")
-time4 = px.histogram(time4data[time4data["Date"] > ROLLING_DATE], x="Date", y="Time", color="Group", height=600)
-time4.update_layout(bargap=0.1, title="What do we work on")
 
-# Billable time
-billable = px.histogram(work.data.sort_values("Key"), x="User", y="Billable", color="Key", height=600)
+# =========================================================
+# Table: Projects (team)
+# =========================================================
 
-billable.update_xaxes(categoryorder="total ascending")
 
-# Internal time
-internal = px.histogram(work.data.sort_values("Key"), x="User", y="Internal", color="Key", height=600)
+d_by_group = data.byGroup().sort_values("Group")
+table_projects_team = px.histogram(
+    d_by_group[d_by_group["Date"] > ROLLING_DATE], x="Date", y="Time", color="Group", height=600
+)
+table_projects_team.update_layout(bargap=0.1, title="What do we work on")
 
-internal.update_xaxes(categoryorder="total descending")
 
-# Popular projects
-fig3 = px.histogram(work.data, x="Group", y="Time", color="User", height=600)
+# =========================================================
+# Table: Billable time
+# =========================================================
 
-fig3.update_xaxes(categoryorder="total ascending")
 
-# Eggs and baskets
-daysAgo = 90
-if sd.rates.empty:
+table_billable = px.histogram(data.data.sort_values("Key"), x="User", y="Billable", color="Key", height=600)
+
+table_billable.update_xaxes(categoryorder="total ascending")
+
+
+# =========================================================
+# Table: Internal time
+# =========================================================
+
+
+table_internal = px.histogram(data.data.sort_values("Key"), x="User", y="Internal", color="Key", height=600)
+
+table_internal.update_xaxes(categoryorder="total descending")
+
+
+# =========================================================
+# Table: Popular projects
+# =========================================================
+
+
+table_popular_projects = px.histogram(data.data, x="Group", y="Time", color="User", height=600)
+
+table_popular_projects.update_xaxes(categoryorder="total ascending")
+
+
+# =========================================================
+# Table: Egg baskets
+# =========================================================
+
+
+days_ago = 90
+if supplementary_data.rates.empty:
     yAxisTitle = "Sum of billable time"
-    eggbaskets = px.histogram(work.byTotalGroup(daysAgo), x="Group", y="Billable", color="User")
-    eggbaskets.update_xaxes(categoryorder="total ascending")
+    table_eggbaskets = px.histogram(data.byTotalGroup(days_ago), x="Group", y="Billable", color="User")
+    table_eggbaskets.update_xaxes(categoryorder="total ascending")
 else:
     yAxisTitle = "Sum of Income (Euro)"
-    eggbaskets = px.histogram(
-        work.byEggBaskets(),
+    table_eggbaskets = px.histogram(
+        data.byEggBaskets(),
         x="Group",
         y="Income",
         color="User",
@@ -198,57 +270,56 @@ else:
         facet_col_wrap=3,
         category_orders={"TimeBasket": ["60-90 days ago", "30-60 days ago", "0-30 days ago"]},
     )
-eggbaskets.update_layout(
-    height=600, yaxis_title=yAxisTitle, bargap=0.1, title="Baskets for our eggs (" + str(daysAgo) + " days back)"
+
+table_eggbaskets.update_layout(
+    height=600, yaxis_title=yAxisTitle, bargap=0.1, title="Baskets for our eggs (" + str(days_ago) + " days back)"
 )
 
-#
+
 # =========================================================
-#
+# Rendering
+# =========================================================
+
 
 tabStructure = dcc.Tabs(
     id="tabs-graph",
-    value="table1",
+    value="working_hours",
     children=[
-        dcc.Tab(label="Our hours", value="table1"),
-        dcc.Tab(label="Rates", value="table2"),
+        dcc.Tab(label="Our hours", value="working_hours"),
+        dcc.Tab(label="Rates", value="rates"),
         dcc.Tab(label="Billable", value="billable"),
         dcc.Tab(label="Internal", value="internal"),
-        dcc.Tab(label="Popular projects", value="fig3"),
+        dcc.Tab(label="Popular projects", value="popular_projects"),
         dcc.Tab(label="Projects", value="projects"),
         dcc.Tab(label="Egg Baskets", value="eggbaskets"),
-        dcc.Tab(label="Rolling time", value="RollingTime"),
-        dcc.Tab(label="Rolling income", value="rollingIncome"),
-        dcc.Tab(label="Company weekly income", value="rollingAllIncomeTotal"),
+        dcc.Tab(label="Rolling time", value="rolling_time"),
+        dcc.Tab(label="Rolling income", value="rolling_income"),
+        dcc.Tab(label="Company weekly income", value="rolling_total"),
     ],
 )
 
 pageheader = html.Div(
     [
         dcc.Markdown("## Verifa Metrics Dashboard"),
-        dcc.Markdown(
-            f"""
-    #### For data between {START_DATE} and {TODAY}
-    # """
-        ),
+        dcc.Markdown(f"#### For data between {START_DATE} and {TODAY}"),
     ]
 )
 
 
-tabDict = {
-    "table1": [table1],
-    "table2": [table2],
-    "billable": [billable],
-    "internal": [internal],
-    "fig3": [fig3],
-    "projects": [time4, time3],
-    "eggbaskets": [eggbaskets],
-    "RollingTime": [rollingTeamAverage, rollingAll],
-    "rollingIncome": [rollingTeamAverageIncome, rollingAllIncome],
-    "rollingAllIncomeTotal": [rollingAllIncomeTotal],
+figure_tabs = {
+    "working_hours": [table_working_hours],
+    "rates": [table_rates],
+    "billable": [table_billable],
+    "internal": [table_internal],
+    "popular_projects": [table_popular_projects],
+    "projects": [table_projects_team, table_projects_individual],
+    "eggbaskets": [table_eggbaskets],
+    "rolling_time": [table_rolling_time_team, table_rolling_time_individual],
+    "rolling_income": [table_rolling_income_team, table_rolling_income_individual],
+    "rolling_total": [table_rolling_total],
 }
 
 
 def render_content(tab):
-    sections = [dcc.Graph(id="plot", figure=x) for x in tabDict[tab]]
+    sections = [dcc.Graph(id="plot", figure=figure) for figure in figure_tabs[tab]]
     return html.Div(html.Section(children=sections))
