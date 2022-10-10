5667f9cfd04b61b7c2e55679d488ddac78f74b78
Bosse Nyström <bon@praqma.net>
2022-10-10 14:56:28 +0200

    imports numpy as np (#138)

--- tests/test_busday.py
+++ tests/test_busday.py
@@ -1,22 +1,23 @@
+""" Some simple tests for busday"""
 import unittest
 
-import numpy
+import numpy as np
 
 
 class TestBusday(unittest.TestCase):
-    """some simple tests for numpy.busday"""
+    """some simple tests for np.busday"""
 
     def test_one_day_weekend(self):
-        self.assertEqual(numpy.busday_count("2022-01-01", "2022-01-02", weekmask="1111100"), 0, "Should be 0")
+        self.assertEqual(np.busday_count("2022-01-01", "2022-01-02", weekmask="1111100"), 0, "Should be 0")
 
     def test_one_day_week(self):
-        self.assertEqual(numpy.busday_count("2022-01-03", "2022-01-04", weekmask="1111100"), 1, "Should be 1")
+        self.assertEqual(np.busday_count("2022-01-03", "2022-01-04", weekmask="1111100"), 1, "Should be 1")
 
     def test_one_week(self):
-        self.assertEqual(numpy.busday_count("2022-01-01", "2022-01-07", weekmask="1111100"), 4, "Should be 4")
+        self.assertEqual(np.busday_count("2022-01-01", "2022-01-07", weekmask="1111100"), 4, "Should be 4")
 
     def test_two_weeks(self):
-        self.assertEqual(numpy.busday_count("2022-01-01", "2022-01-14", weekmask="1111100"), 9, "Should be 9")
+        self.assertEqual(np.busday_count("2022-01-01", "2022-01-14", weekmask="1111100"), 9, "Should be 9")
 
 
 if __name__ == "__main__":
