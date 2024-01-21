9f136f06446ac7e1004e8c9a76c0bd1ab33fadab
Bosse Nyström <bosse.26773@gmail.com>
2024-01-21 11:28:36 +0100

    For #235 - flips egg baskets to be by user, and add costs per consultant

--- metrics/notion.py
+++ metrics/notion.py
@@ -91,16 +91,17 @@ class Crew(Notion):
 
     def get_crew(self) -> None:
         result_dict = self.fetch_data(self.database_id).json()
-        data = pd.DataFrame(columns=["User", "Total cost"])
+        data = pd.DataFrame(columns=["User", "Role", "Total cost"])
 
         for item in result_dict["results"]:
             user = item["properties"]["Person"]["people"][0]["name"]
+            role = item["properties"]["Role"]["select"]["name"]
             currency = item["properties"]["Currency"]["select"]["name"]
             cost = item["properties"]["Total Cost"]["number"] / (
                 11.43 if currency == "SEK" else 1
             )  # TODO: constant or helper method for SEK to EUR
 
-            data.loc[-1] = [user, cost]
+            data.loc[-1] = [user, role, cost]
             data.index = data.index + 1
 
         self.data = data.sort_values(by=["User"])
